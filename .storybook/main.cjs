6fff374ab6da60ba6bc3b418e54a4abc93d27498
Guilhermecheng <62719629+Guilhermecheng@users.noreply.github.com>
2022-10-12 23:02:50 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,5 +15,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'production') {
+      config.base = '/lab-designsystem/'
+    }
+
+    return config
   }
 }
